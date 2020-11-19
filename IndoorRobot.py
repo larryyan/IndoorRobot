07d29e2c6c06266deefb073bd1e37a7cd7e50680
larryyan <35266037+larryyan@users.noreply.github.com>
2020-11-19 08:37:38 +0000

    add pathplanning

---
+++ IndoorRobot.py
@@ -0,0 +1,22 @@
+'''
+author:     严嘉哲
+date:       2020/11/7
+program:    室内引导机器人
+'''
+
+# 此处import机器人lab同学的单片机接口
+
+# 此处import行进的程序
+
+# 以下为本程序所需的库
+
+
+
+def main():
+    
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    pass
