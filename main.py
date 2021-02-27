36539f1d2896176d0698c967141997ece1275d3d
Warren <2966094090@qq.com>
2021-02-27 10:57:19 +0800

    0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #luncher of GUI
 import os
+import sys
 text='==============================\n|Select GUI:                 |\n|"0" for Beta version        |\n|"1" for Origin version      |\n=============================='
 print(text)
 Select = int(input())
@@ -10,4 +11,8 @@ while 1:
         os.system('python Interface.py')
     else:
         print('pls try again\n',text)
-    Select = int(input())
+    try:
+        Select = int(input())
+    except Exception:
+        sys.exit(1)
+        
